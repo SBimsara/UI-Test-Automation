d19630f6dab3ef2ce019eff8108dcbafd443d116
Nishath Ahamed <117764163+NishathAhamed@users.noreply.github.com>
2024-12-28 12:02:10 +0530

    Handling Overlapping Leave Dates

---
+++ cypress/e2e/features/leave/leave-application-overlapping-dates.feature
@@ -0,0 +1,16 @@
+Feature: Handling Overlapping Leave Dates
+
+  Background:
+    Given User is logged in for handling overlapping leave dates scenario
+
+  Scenario: User applies for overlapping leave dates
+    Given the user is on the Apply Leave page, for overlapping leave dates
+    When the user selects the Leave Type dropdown, for overlapping leave dates
+    And the user enters "2024-01-03" as the From Date, for overlapping leave dates
+    And the user enters "2024-01-03" as the To Date, for overlapping leave dates
+    And the user clicks the Apply button, for overlapping leave dates
+    When the user selects the Leave Type dropdown, for overlapping leave dates
+    And the user enters "2024-01-03" as the From Date, for overlapping leave dates
+    And the user enters "2024-01-03" as the To Date, for overlapping leave dates
+    And the user clicks the Apply button, for overlapping leave dates
+    Then an error message should be displayed indicating that the leave dates overlap, for overlapping leave dates
--- cypress/support/step_definitions/apply-leave-successful.js
+++ cypress/support/step_definitions/apply-leave-successful.js
@@ -5,13 +5,57 @@ Given('User is logged in', () => {
     cy.login();
 });
 
+Given('delete the leave list', () => {
+  
+  cy.visit('/web/index.php/leave/viewMyLeaveList');
+  //set from date
+
+  cy.xpath('//*[@id="app"]/div[1]/div[2]/div[2]/div/div[1]/div[2]/form/div[1]/div/div[1]/div/div[2]/div/div/input')
+    .first().clear().type('2024-01-01').click(); 
+
+  //set to date
+  cy.xpath('//*[@id="app"]/div[1]/div[2]/div[2]/div/div[1]/div[2]/form/div[1]/div/div[2]/div/div[2]/div/div/input')
+  .last().clear().type('2026-31-12').click(); 
+
+  //remove rejected
+  cy.xpath('//*[@id="app"]/div[1]/div[2]/div[2]/div/div[1]/div[2]/form/div[1]/div/div[3]/div/div[2]/div/div[2]/span[1]/i').click();
+  //remove cancelled
+  cy.xpath('//*[@id="app"]/div[1]/div[2]/div[2]/div/div[1]/div[2]/form/div[1]/div/div[3]/div/div[2]/div/div[2]/span[1]/i').click();
+  //remove Sceduled
+  cy.xpath('//*[@id="app"]/div[1]/div[2]/div[2]/div/div[1]/div[2]/form/div[1]/div/div[3]/div/div[2]/div/div[2]/span[2]/i').click();
+  //remove Taken
+  cy.xpath('//*[@id="app"]/div[1]/div[2]/div[2]/div/div[1]/div[2]/form/div[1]/div/div[3]/div/div[2]/div/div[2]/span[2]/i').click();
+
+
+  //Click the Search button
+  cy.get('[data-v-10d463b7]').contains('Search').click(); 
+
+  cy.get('.oxd-table-body').then((tableBody) => {
+    if (tableBody.children().length === 0) {
+      cy.log('The table body is empty');
+    } else {
+      cy.log('The table body is not empty');
+      // No "No Records Found" text exists, proceed with deletion
+      cy.xpath('//*[@id="app"]/div[1]/div[2]/div[2]/div/div[2]/div[2]/div/div[1]/div/div[1]/div/label/span/i').click();
+      cy.xpath('//*[@id="app"]/div[1]/div[2]/div[2]/div/div[2]/div[1]/div/button').click();
+  
+      // Click confirm to delete pending leave records
+      cy.xpath('//*[@id="app"]/div[3]/div/div/div/div[3]/button[2]').click();
+    }
+  });
+
+});
+
+
+
 Given('the user navigates to the Apply Leave page', () => {
   cy.visit('/web/index.php/leave/applyLeave');
 });
 
+
 When('the user selects {string} from the Leave Type dropdown', (leaveType) => {
   cy.get('.oxd-select-wrapper').click();  
-  cy.get('span[data-v-13cf171c=""]').click();
+  cy.xpath('//*[@id="app"]/div[1]/div[2]/div[2]/div/div/form/div[1]/div/div[1]/div/div[2]/div/div[2]/div[2]').click();
   cy.get('.oxd-select-text-input').should('not.be.empty');
 });
 
@@ -29,13 +73,6 @@ When('the user enters {string} as the To Date', (toDate) => {
     .last().clear().type(toDate).click(); 
 });
 
-When('the user selects {string} from the Duration dropdown', (duration) => {
-  cy.xpath('//*[@id="app"]/div[1]/div[2]/div[2]/div/div/form/div[3]/div/div/div/div[2]/div/div/div[2]/i')
-    .should('be.visible').type(duration).click(); 
-  cy.xpath('//*[@id="app"]/div[1]/div[2]/div[2]/div/div/form/div[3]/div/div/div/div[2]/div/div/div[1]')
-    .should('not.be.empty');
-});
-
 When('the user adds {string} in the Comments', (comment) => {
   cy.xpath('//*[@id="app"]/div[1]/div[2]/div[2]/div/div/form/div[4]/div/div/div/div[2]')
     .should('be.visible').and('not.be.disabled').type(comment);
@@ -47,4 +84,6 @@ When('the user clicks the Apply button', () => {
 
 Then('the leave request should be submitted successfully', () => {
   cy.contains('Success').should('be.visible');
+
+  
 });
