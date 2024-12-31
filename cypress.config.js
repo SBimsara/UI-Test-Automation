4be2cceb6ea802c26500325c4fef138c3d8823f7
Sithira Bimsara <basnayakesb.20@uom.lk>
2024-12-31 20:13:57 +0530

    added allure reporting with version downgrades

--- cypress.config.js
+++ cypress.config.js
@@ -2,17 +2,29 @@ const { defineConfig } = require("cypress");
 const createBundler = require("@bahmutov/cypress-esbuild-preprocessor");
 const preprocessor = require("@badeball/cypress-cucumber-preprocessor");
 const createEsbuildPlugin = require("@badeball/cypress-cucumber-preprocessor/esbuild");
-
+const allureWriter = require('@shelex/cypress-allure-plugin/writer');
 module.exports = defineConfig({
   e2e: {
     setupNodeEvents(on, config) {
+
+      preprocessor.addCucumberPreprocessorPlugin(on, config);
+
       on("file:preprocessor",
       createBundler({
         plugins: [createEsbuildPlugin.default(config)],
       }));
-      preprocessor.addCucumberPreprocessorPlugin(on, config);
+
+      allureWriter(on, config);
+
       return config;
+
     },
+    env: {
+      allureResultsPath: "allure-results",
+      allureReuseAfterSpec: true,
+      allure: true
+    },
+
     specPattern: [
       "**/*.feature",
     ],
