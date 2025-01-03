554f18eadf6d1116c095c917141c38e6c96f29cd
Sithira Bimsara <basnayakesb.20@uom.lk>
2025-01-03 23:15:23 +0530

    added video and screenshot for reports

--- cypress.config.js
+++ cypress.config.js
@@ -3,6 +3,8 @@ const createBundler = require("@bahmutov/cypress-esbuild-preprocessor");
 const preprocessor = require("@badeball/cypress-cucumber-preprocessor");
 const createEsbuildPlugin = require("@badeball/cypress-cucumber-preprocessor/esbuild");
 const allureWriter = require('@shelex/cypress-allure-plugin/writer');
+const { readFileSync } = require('fs');
+
 module.exports = defineConfig({
   e2e: {
     setupNodeEvents(on, config) {
@@ -16,6 +18,16 @@ module.exports = defineConfig({
 
       allureWriter(on, config);
 
+      // // Attach video to Allure report after each spec
+      on('after:spec', (spec, results) => {
+        if (results && results.video) {
+          const videoPath = results.video;
+          // Read the video file and attach it to Allure
+          const videoContent = readFileSync(videoPath);
+          cy.allure().fileAttachment('Test Video', videoContent, 'video/mp4');
+        }
+      });
+
       return config;
 
     },
@@ -24,6 +36,11 @@ module.exports = defineConfig({
       allureReuseAfterSpec: true,
       allure: true
     },
+    screenshotOnRunFailure: true,
+
+    video: true,
+    videoCompression: false, // Disable compression for faster video creation
+    videoUploadOnPasses: true, // Enable video creation even for passing tests
 
     specPattern: [
       "**/*.feature",
